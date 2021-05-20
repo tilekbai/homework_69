fab40db1a3d8ef278c8f0133c0f6d25e5aa0be62
tilekbai <kukushokk@gmail.com>
2021-05-20 17:10:20 +0600

    Созданы представления для вычислений

--- source/api_app_1/views.py
+++ source/api_app_1/views.py
@@ -1,3 +1,96 @@
-from django.shortcuts import render
 
-# Create your views here.
+import json
+from datetime import datetime
+from django.http import HttpResponse, HttpResponseNotAllowed, response
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+def json_echo_view(request, *args, **kwargs):
+    answer = {
+        'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'method': request.method,
+    }    
+    if request.body:
+        answer['content'] = json.loads(request.body)
+    answer_as_json = json.dumps(answer)
+    response = HttpResponse(answer_as_json)
+    response['Content-Type'] = 'application/json'
+    return response
+
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed('Only GET request are allowed')
+
+
+#######################################################
+numdict = {
+     "A": 1234,
+     "B": 4567
+}
+
+err_dict = {
+            "error": "TypeError: must be int!"
+        }
+
+def add_view(request, *args, **kwargs):
+    if type(numdict["A"]) == int and type(numdict["B"]) == int:
+        sumres = numdict["A"] + numdict["B"]
+        sumres_as_json = json.dumps(sumres)
+        response = HttpResponse(sumres_as_json)
+        response['Content-Type'] = 'application/json'
+        return response
+    else:
+        error = json.dumps(err_dict)
+        response = HttpResponse(error)
+        response.status_code = 400
+        return response
+
+def subtract_view(request, *args, **kwargs):
+    if type(numdict["A"]) == int and type(numdict["B"]) == int:
+        subres = numdict["A"] - numdict["B"]
+        subres_as_json = json.dumps(subres)
+        response = HttpResponse(subres_as_json)
+        response['Content-Type'] = 'application/json'
+        return response
+    else:
+        error = json.dumps(err_dict)
+        response = HttpResponse(error)
+        response.status_code = 400
+        return response
+
+def multiply_view(request, *args, **kwargs):
+    if type(numdict["A"]) == int and type(numdict["B"]) == int:
+        sumres = numdict["A"] * numdict["B"]
+        sumres_as_json = json.dumps(sumres)
+        response = HttpResponse(sumres_as_json)
+        response['Content-Type'] = 'application/json'
+        return response
+    else:
+        error = json.dumps(err_dict)
+        response = HttpResponse(error)
+        response.status_code = 400
+        return response
+
+def divide_view(request, *args, **kwargs):
+    if type(numdict["A"]) == int and type(numdict["B"]) == int:
+        if numdict["B"] != 0:
+            sumres = numdict["A"] / numdict["B"]
+            sumres_as_json = json.dumps(sumres)
+            response = HttpResponse(sumres_as_json)
+            response['Content-Type'] = 'application/json'
+            return response
+        else:
+            err_dict["error"] = "Division by zero!"
+            error = json.dumps(err_dict)
+            response = HttpResponse(error)
+            response.status_code = 400
+            return response
+
+    else:
+        error = json.dumps(err_dict)
+        response = HttpResponse(error)
+        response.status_code = 400
+        return response
+
--- source/project/urls.py
+++ source/project/urls.py
@@ -1,21 +1,21 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from api_app_1.views import (
+    json_echo_view, 
+    get_token_view,
+    add_view,
+    subtract_view,
+    multiply_view,
+    divide_view,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', json_echo_view),
+    path('test/coocki/', get_token_view),
+    path('add/', add_view),
+    path('subtract/', subtract_view),
+    path('multiply/', multiply_view),
+    path('divide/', divide_view),
 ]
